410cbd967d2835afc3838c69b6febcd111905754
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-06-10 17:28:38 -0400

    task 5 complete

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ Your function should accept:
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
 function getFlavorByIndex(arr, index){
-        console.log(arr[index]);
+    console.log(arr[index]);
 }
 
 getFlavorByIndex(originalFlavors, 2);
@@ -114,12 +114,15 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
-
+function removeFlavorByName(arr,flavor){
+    var removeThis = arr.indexOf(flavor);
+    if(removeThis > -1) {
+        arr.splice(removeThis, 1);
+    }
+    console.log(arr);
 }
 
+removeFlavorByName(originalFlavors, "Vanilla");
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
